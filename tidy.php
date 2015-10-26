4afdf04cdd451dfc9de7b5487227d6a2d7209640
Daniel Fredriksen <dfredriksen@dnainfo.com>
2015-10-26 15:08:55 -0400

    Experiment 1

--- tidy.php
+++ tidy.php
@@ -23,3 +23,28 @@ while( $row = $result->fetch(PDO::FETCH_ASSOC))
 }
 
 $db->exec("DELETE FROM Top10 where pathTail IS NULL");
+
+$result = $db->query("
+    SELECT m.reference_id, s.headline, s.teaser, t.row_id from Analysis.Top10 t
+    inner join Main.Mapper m on m.path like CONCAT('%', t.pathTail, '%')
+    inner join Main.StoryMain s on m.reference_id = s.story_id 
+    WHERE m.site_id = 1 AND t.story_id = 0
+    ");
+while( $row = $result->fetch(PDO::FETCH_ASSOC))
+{
+    $story_id = $row['reference_id'];
+    $headline = $row['headline'];
+    $teaser = $row['teaser'];
+    $id = $row['row_id'];
+
+    echo "$story_id: $headline\n";
+
+    $statement = $db->prepare("UPDATE Top10 set story_id = :story_id, headline=:headline, teaser=:teaser where row_id=:id");
+    $statement->bindParam(':story_id', $story_id, PDO::PARAM_INT);
+    $statement->bindParam(':headline', $headline);
+    $statement->bindParam(':teaser', $teaser);
+    $statement->bindParam(':id', $id);
+    $statement->execute();
+}
+
+$db->exec("DELETE FROM Top10 where story_id = 0");
